46575e3d11ceebf48a220b70545de6d8a021d923
Jorge Ivan Gomez <jorgomezre@unal.edu.co>
2022-05-21 20:03:08 -0500

    Creacion pagina registro sin estilos

--- app/src/main/java/com/android/example/finalprojectbootcamp/MainActivity.kt
+++ app/src/main/java/com/android/example/finalprojectbootcamp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.android.example.finalprojectbootcamp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)
+        setContentView(R.layout.registro)
     }
 }
