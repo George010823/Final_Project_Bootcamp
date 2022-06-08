f429b4b0d72ea56768d20859d39003dffb73163b
Jorge Ivan Gomez <jorgomezre@unal.edu.co>
2022-06-08 13:51:04 -0500

    correccion 08062022

--- app/src/main/java/com/android/example/finalprojectbootcamp/LoginActivity.kt
+++ app/src/main/java/com/android/example/finalprojectbootcamp/LoginActivity.kt
@@ -1,11 +1,31 @@
 package com.android.example.finalprojectbootcamp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.example.finalprojectbootcamp.databinding.LoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: LoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)
+        binding = LoginBinding.inflate(layoutInflater)
+
+        binding.btnLogin.setOnClickListener{openSearchActivity()}
+
+        setContentView(binding.root) //LO ULTIMO EN GUARACHA
+        //setContentView(R.layout.login) ANTES
+
+
     }
+    private fun openSearchActivity(){
+
+        Toast.makeText(this, "si pasa por aca", Toast.LENGTH_LONG).show()
+        Log.i("INFO", "BOTON LOGIN")
+        val goSearch = Intent(this, SearchActivity::class.java)
+        startActivity(goSearch)
+        }
 }
