fdbc03db191084fe0e618c8a893a1a112cca3895
Jorge Ivan Gomez <jorgomezre@unal.edu.co>
2022-06-19 12:42:09 -0500

    Recycler_view_Search OK

--- app/src/main/java/com/android/example/finalprojectbootcamp/SearchActivity.kt
+++ app/src/main/java/com/android/example/finalprojectbootcamp/SearchActivity.kt
@@ -4,10 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.android.example.finalprojectbootcamp.adapter.MovieAdapter
-import com.android.example.finalprojectbootcamp.data.MoviesProvider
 import com.android.example.finalprojectbootcamp.databinding.SearchBinding
 
 class SearchActivity : AppCompatActivity() {
@@ -19,14 +15,7 @@ class SearchActivity : AppCompatActivity() {
         binding = SearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initRecyclerView()
         seleccionarItemBar()
-    }
-
-    private fun initRecyclerView() {
-        val recyclerView = findViewById<RecyclerView>(R.id.listPeliculas)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = MovieAdapter(MoviesProvider.movieList)
     }
 
     private fun seleccionarItemBar() {
--- app/src/main/java/com/android/example/finalprojectbootcamp/SearchFragment.kt
+++ app/src/main/java/com/android/example/finalprojectbootcamp/SearchFragment.kt
@@ -1,12 +1,20 @@
 package com.android.example.finalprojectbootcamp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import android.widget.Toast.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.example.finalprojectbootcamp.adapter.MovieAdapter
+import com.android.example.finalprojectbootcamp.data.MoviesProvider
+import com.android.example.finalprojectbootcamp.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
+    private lateinit var binding: FragmentSearchBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +24,23 @@ class SearchFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+
+        Toast.makeText(context, "FRAGMENTSEARCH", LENGTH_LONG).show()
+
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        Log.i("INFO", "INICIAR RECYCLERVIEW")
+
+        binding.listPeliculas.layoutManager = LinearLayoutManager(context)
+        binding.listPeliculas.adapter = MovieAdapter(MoviesProvider.movieList)
     }
 }
